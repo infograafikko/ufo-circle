86ccb5dbd564d102371a38479ac2edfe7d18415d
infograafikko <joel@infograafikko.fi>
2017-01-10 22:49:06 +0200

    Add files via upload

--- script/radial3.js
+++ script/radial3.js
@@ -318,11 +318,11 @@
                 var lastY;
                 $('svg').bind('touchmove', function (e){
                      var currentY = e.originalEvent.touches[0].clientY;
-                     if(currentY > lastY + 1){
+                     if(currentY > lastY + 2){
                          // moved down
                          witcher = true;
                          window.setTimeout(rotationFunction(), 1000)
-                     }else if(currentY < lastY - 1){
+                     }else if(currentY < lastY - 2){
                          // moved up
                          switcher = false;
                          window.setTimeout(rotationFunction(), 1000)
